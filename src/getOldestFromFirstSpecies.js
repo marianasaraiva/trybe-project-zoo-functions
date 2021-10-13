207723b43c980c9e5e5f4d8d766a4d192db456d6
Mariana Saraiva <marianascm8@gmail.com>
2021-10-13 16:58:04 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const teste = data.employees.find((func) => func.id === id).responsibleFor[0];
+  const animalObj = data.species.find((specie) => specie.id === teste).residents;
+  return Object.values(animalObj.sort((a, b) => a.age - b.age)[animalObj.length - 1]);
 }
+
+// console.log(getOldestFromFirstSpecies(burlId));
 
 module.exports = getOldestFromFirstSpecies;
