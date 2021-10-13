39b85e4dc318e623d341dc1f139222bffddb958c
Mariana Saraiva <marianascm8@gmail.com>
2021-10-13 15:23:38 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  console.log(ids);
+  return species.filter((animal) => ids.some((id) => animal.id === id));
 }
 
 module.exports = getSpeciesByIds;
