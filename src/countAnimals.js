6f34c5f816b06d7ce0d6855ab3b3a79a50d2e758
Mariana Saraiva <marianascm8@gmail.com>
2021-10-13 16:45:58 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const teste = () => {
+const objetoQtdAnimais = () => {
   const objeto = species.reduce((acc, specie) => {
     acc[specie.name] = specie.residents.length;
     return acc;
@@ -10,10 +10,11 @@ const teste = () => {
   return objeto;
 };
 
-const teste2 = (animal) => species.reduce((sum, spec) => ((spec.name === animal.specie)
-  ? (spec.residents) : (sum)), 0).length;
+const qtdAnimaisResidentes = (animal) => species
+  .reduce((sum, spec) => ((spec.name === animal.specie)
+    ? (spec.residents) : (sum)), 0).length;
 
-const teste3 = (animal) => {
+const qtdAnimaisSexo = (animal) => {
   const ani = species.find((specie) => specie.name === animal.specie).residents;
   const animals = ani.reduce((acc, sexo) => ((sexo.sex === animal.sex)
     ? (acc + 1) : (acc)), 0);
@@ -21,19 +22,13 @@ const teste3 = (animal) => {
 };
 
 function countAnimals(animal) {
-  if (animal === undefined) { // Utilizando a HOF reduce para reduzir o array para objeto no formato desejado.
-    return teste();
+  if (animal === undefined) {
+    return objetoQtdAnimais();
   }
-  if (Object.values(animal).length === 1) { // Usei a HOF reduce para informar o numero de animais por nome e o length para dizer o tamanho do array.
-    return teste2(animal);
+  if (Object.values(animal).length === 1) {
+    return qtdAnimaisResidentes(animal);
   }
-  return teste3(animal);
+  return qtdAnimaisSexo(animal);
 }
-
-console.log(countAnimals());
-// console.log(countAnimals({ specie: 'giraffes' }));
-// console.log(countAnimals({ specie: 'penguins' }));
-console.log(countAnimals({ specie: 'bears', sex: 'female' })); // 0
-// console.log(countAnimals({ specie: 'elephants', sex: 'male' })); // 2
 
 module.exports = countAnimals;
