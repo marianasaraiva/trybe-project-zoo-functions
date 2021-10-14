b656eda6e4ef2a6b2e4eb2d7f16b78090c704862
Mariana Saraiva <marianascm8@gmail.com>
2021-10-14 18:48:50 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const arrayResultado = employees.map((objeto) => {
+  const arr = {
+    id: objeto.id,
+    fullName: `${objeto.firstName} ${objeto.lastName}`,
+    species: objeto.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).name),
+    locations: objeto.responsibleFor.map((id) => species
+      .find((specie) => specie.id === id).location),
+  };
+  return arr;
+});
+
+function getEmployeesCoverage(parametro) {
+  if (parametro === undefined) return arrayResultado;
+  const validarEntrada = arrayResultado
+    .find((element) => element.fullName.includes(Object.values(parametro))
+      || element.id.includes(Object.values(parametro)));
+  if (validarEntrada === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return validarEntrada;
 }
 
 module.exports = getEmployeesCoverage;
